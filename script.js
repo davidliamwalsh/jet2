82742b95bffcc9043401d1b3d1067545b0194443
david <david@david-rMBP.local>
2019-04-22 01:17:36 +0100

    final push

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const modal = document.querySelector("button[data-behavior='modalButton']")
 const modalView = document.querySelector("div[data-behavior='modalView']")
-const closeModal = document.querySelector("button[data-behavior='closeModal']")
-const continueModal = document.querySelector("button[data-behavior='continueModal']")
+const closeBtn = document.querySelector("button[data-behavior='closeBtn']")
+const continueBtn = document.querySelector("button[data-behavior='continueBtn']")
 
 const openModal = (event) => {
   modalView.style.display = 'block'
@@ -11,6 +11,10 @@ const clear = (event) => {
   modalView.style.display = 'none'
 }
 
+const redirect = (event) => {
+  window.location.href = "https://www.jet2.com";
+}
+
 const outsideClick = (event) => {
   if(event.target == modalView) {
     modalView.style.display = 'none'
@@ -18,5 +22,6 @@ const outsideClick = (event) => {
 }
 
 modal.addEventListener('click', openModal)
-closeModal.addEventListener('click', clear)
+closeBtn.addEventListener('click', clear)
+continueBtn.addEventListener('click', redirect)
 window.addEventListener('click', outsideClick)
